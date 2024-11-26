45220eaebeca506ac31feb6371c8c2ac8d1f3662
chagit03440 <82941665+chagit03440@users.noreply.github.com>
2024-11-26 15:02:44 +0200

    fix joblist

--- src/app/components/JobList.tsx
+++ src/app/components/JobList.tsx
@@ -3,21 +3,17 @@ import IJob from '../types/job';
 import { useQuery } from '@tanstack/react-query';
 import JobCard from './JobCard';
 import CandidateJobCard from './CandidateJobCard';
-
-const getAllJobs = async (): Promise<IJob[]> => {
-  const response = await fetch('/api/jobs'); // Adjust the endpoint accordingly
-  const data = await response.json();
-  return data;
-};
+import { getJobs } from '../services/jobServices';
 
 interface JobListProps {
   userRole: 'candidate' | 'employee';  // Pass user role as a prop
 }
 
 const JobList: React.FC<JobListProps> = ({ userRole }) => {
-  const { data: jobs, isLoading, error } = useQuery<IJob[]>({
+    
+  const { data: jobs=[], isLoading, error } = useQuery<IJob[]>({
     queryKey: ['jobs'],
-    queryFn: getAllJobs,
+    queryFn: getJobs,
   });
 
   if (isLoading) return <div>Loading...</div>;
@@ -26,13 +22,13 @@ const JobList: React.FC<JobListProps> = ({ userRole }) => {
   return (
     <div className="w-full max-w-4xl mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {jobs?.map((job) =>
-          userRole === 'candidate' ? (
+      {jobs?.map((job) => (
+        userRole === 'candidate' ? (
             <CandidateJobCard key={job._id} job={job} />
-          ) : (
+        ) : (
             <JobCard key={job._id} job={job} />
-          )
-        )}
+        )
+        )) || <div>No jobs available</div>}
       </div>
     </div>
   );
