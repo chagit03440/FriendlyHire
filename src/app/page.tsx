0a8749228462aa07786de39e2e76515e918295ab
Shirel Zohar <83185587+Shirel1234@users.noreply.github.com>
2024-11-26 15:06:56 +0200

    fix navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+'use client'
+import { useRoleStore } from "./store/userStore";
 
 export default function Home() {
+  const updateRole= useRoleStore ((state) => state.updateRole);
+  updateRole("Login");
   return (
     <div>
 
