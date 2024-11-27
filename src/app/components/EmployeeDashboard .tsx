1c7eff07d3908f47f472b08deb83e99e0f879d97
chagit03440 <82941665+chagit03440@users.noreply.github.com>
2024-11-27 10:41:06 +0200

    fix joblist

--- src/app/components/EmployeeDashboard .tsx
+++ src/app/components/EmployeeDashboard .tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import JobList from './JobList'; 
 
 const EmployeeDashboard  = () => {
   return (
-    <div>
-      
+    <div className="employee-page">
+      <h1 className="text-2xl font-bold">Available Jobs for Employees</h1>
+      <JobList userRole="employee" /> {/* Passing role as 'employee' */}
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 import "./globals.css";
 import NavBar from "./components/NavBar";
-import { useRoleStore } from "./store/userStore";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -11,9 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const [queryClient] = useState(() => new QueryClient());
-  const updateRole = useRoleStore((state) => state.updateRole);
-
-  updateRole("Login");
+ 
 
   return (
     <html lang="en">
