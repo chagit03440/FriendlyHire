ff8b53e3776656cc9eeeac3a5e4dadb31a419675
Shirel Zohar <83185587+Shirel1234@users.noreply.github.com>
2024-12-02 10:48:14 +0200

    fix structure

--- src/app/context/UserContext.tsx
+++ src/app/context/UserContext.tsx
@@ -4,15 +4,18 @@ import React, { createContext, useContext, useState } from 'react';
 interface UserContextType {
     role: string | null;
     setRole: (role: string | null) => void;
+    mail: string | null;
+    setMail: (mail: string | null) => void;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [role, setRole] = useState<string | null>(null);
+    const [mail, setMail] = useState<string | null>(null);
 
     return (
-        <UserContext.Provider value={{ role, setRole }}>
+        <UserContext.Provider value={{ role, setRole, mail, setMail }}>
             {children}
         </UserContext.Provider>
     );
