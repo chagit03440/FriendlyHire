5e719081080c254a6b7631d5690d67b49c928467
Shirel Zohar <83185587+Shirel1234@users.noreply.github.com>
2024-12-16 15:04:17 +0200

    feat logic of parse pdf

--- src/app/lib/parseResumeFromPdf/readPdf.ts
+++ src/app/lib/parseResumeFromPdf/readPdf.ts
@@ -1,7 +1,7 @@
 // Getting pdfjs to work is tricky. The following 3 lines would make it work
 // https://stackoverflow.com/a/63486898/7699841
-import * as pdfjs from "pdfjs-dist";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+import * as pdfjs from "pdfjs-dist/legacy/build/pdf";
+import pdfjsWorker from "pdfjs-dist/legacy/build/pdf.worker.entry";
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 import type { TextItem as PdfjsTextItem } from "pdfjs-dist/types/src/display/api";
 import { TextItem, TextItems } from "./types";
--- src/app/types/pdfjs-dist.d.ts
+++ src/app/types/pdfjs-dist.d.ts
@@ -1,5 +1,4 @@
-declare module "pdfjs-dist/build/pdf.worker.entry" {
-    const pdfjsWorker: string;
-    export default pdfjsWorker;
-  }
-  
+declare module "pdfjs-dist/legacy/build/pdf.worker.entry" {
+  const workerSrc: string;
+  export default workerSrc;
+}
