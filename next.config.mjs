cf5f424e271b8f355c047e5963263c42d753dde7
Shirel Zohar <83185587+Shirel1234@users.noreply.github.com>
2024-12-17 12:14:26 +0200

    fix logic parse pdf for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false, // Ignore the 'canvas' module
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
