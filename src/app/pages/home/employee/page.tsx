79ea25711c97bb7b93cb6f99d3209b599b72d391
chagit03440 <82941665+chagit03440@users.noreply.github.com>
2024-12-26 13:35:37 +0200

    fix joblist

--- src/app/pages/home/employee/page.tsx
+++ src/app/pages/home/employee/page.tsx
@@ -1,46 +1,46 @@
-// "use client";
-// import EmployeeDashboard from "@/app/components/employee/EmployeeDashboard ";
-// import React from "react";
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/navigation";
-// import checkAccess from "@/app/utils/checkAccess";
-// import { JobActionsProvider } from "@/app/store/JobActionsContext";
+"use client";
+import EmployeeDashboard from "@/app/components/employee/EmployeeDashboard ";
+import React from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import checkAccess from "@/app/utils/checkAccess";
+import { JobActionsProvider } from "@/app/store/JobActionsContext";
 
-// const Page = () => {
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-//   const router = useRouter();
+const Page = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
 
-//   useEffect(() => {
-//     const validateAccess = async () => {
-//       try {
-//         const userData = await checkAccess();
-//         if (!userData.hasAccess) {
-//           router.push("/pages/login");
-//         } else if (userData.role.toLowerCase() !== "employee") {
-//           router.push("/pages/home");
-//         } else {
-//           setIsAuthenticated(true);
-//         }
-//       } catch (error) {
-//         console.error(error);
-//         router.push("/pages/login");
-//       }
-//     };
+  useEffect(() => {
+    const validateAccess = async () => {
+      try {
+        const userData = await checkAccess();
+        if (!userData.hasAccess) {
+          router.push("/pages/login");
+        } else if (userData.role.toLowerCase() !== "employee") {
+          router.push("/pages/home");
+        } else {
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        console.error(error);
+        router.push("/pages/login");
+      }
+    };
 
-//     validateAccess();
-//   }, [router]);
+    validateAccess();
+  }, [router]);
 
-//   if (!isAuthenticated) {
-//     return <p>...טוען</p>;
-//   }
+  if (!isAuthenticated) {
+    return <p>...טוען</p>;
+  }
 
-//   return (
-//     <JobActionsProvider>
-//       <div>
-//         <EmployeeDashboard />
-//       </div>
-//     </JobActionsProvider>
-//   );
-// };
+  return (
+    <JobActionsProvider>
+      <div>
+        <EmployeeDashboard />
+      </div>
+    </JobActionsProvider>
+  );
+};
 
-// export default Page;
+export default Page;
